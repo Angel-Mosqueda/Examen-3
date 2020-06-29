59a03e2e6ab06b237cbdee49bf95270d223ec64b
Ziltoidian <64275149+Ziltoidian@users.noreply.github.com>
2020-06-28 19:11:42 -0500

    changes

--- AngularFrontEnd/src/app/components/atmosfera-api/atmosfera-api.component.ts
+++ AngularFrontEnd/src/app/components/atmosfera-api/atmosfera-api.component.ts
@@ -8,15 +8,18 @@ import { ApiService } from './../../services/api.service';
 })
 export class AtmosferaAPIComponent implements OnInit {
   var1: any;
+  bandera: boolean = false;
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
   }
 
   funcionAPI2() {
+    this.bandera = true;
     this.api.funcionAPI2().subscribe((var1: any[]) => {
         this.var1 = var1;
         console.log(this.var1);
+        console.log(this.var1.results[0].name);
 
     });
   }
--- AngularFrontEnd/src/app/components/nasa-api/nasa-api.component.ts
+++ AngularFrontEnd/src/app/components/nasa-api/nasa-api.component.ts
@@ -7,16 +7,16 @@ import { ApiService } from './../../services/api.service';
   styleUrls: ['./nasa-api.component.css']
 })
 export class NasaAPIComponent implements OnInit {
-  var1: any;
+  var2: any;
   constructor(private api: ApiService) { }
 
   ngOnInit(): void {
   }
 
   funcionAPI1() {
-    this.api.funcionAPI1().subscribe((var1: any[]) => {
-        this.var1 = var1;
-        console.log(this.var1);
+    this.api.funcionAPI1().subscribe((var2: any[]) => {
+        this.var2 = var2;
+        console.log(this.var2);
 
     });
   }
